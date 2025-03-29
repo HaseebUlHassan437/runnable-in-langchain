03351e73cd7734265ee1efe7ba847e68f5b27641
Haseeb Ul hassan <160396893+HaseebUlHassan437@users.noreply.github.com>
2025-03-29 10:53:26 -0700

    Add files via upload

---
+++ runnable_sequence.py
@@ -0,0 +1,26 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+model = ChatGoogleGenerativeAI(model='gemini-1.5-pro')
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+
+print(chain.invoke({'topic':'AI'}))
